baa27a93551fc11fbaecde109e6de3e02ac8ec3d
RicardoPayan <a218206581@unison.mx>
2024-07-10 20:06:33 -0700

    subiendo repo

---
+++ crud_net_8_mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using crud_net_8_mvc.Models;
+
+namespace crud_net_8_mvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+            
+        }
+
+        //Agregar los modelos aqui (Cada modelo corresponder a una tabla en la DB)
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
