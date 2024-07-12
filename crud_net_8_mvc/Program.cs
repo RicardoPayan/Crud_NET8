65df6518ab9ebfd7d2a8529a0b910b2c85d7f557
RicardoPayan <a218206581@unison.mx>
2024-07-11 22:04:22 -0700

    Migracion de la base de datos

--- crud_net_8_mvc/Program.cs
+++ crud_net_8_mvc/Program.cs
@@ -1,4 +1,11 @@
+using crud_net_8_mvc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Configuramos la conexion sql ser local db MSSQLLOCAL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
